a88012c4850f21785c53943d61a7283fa21cbbbf
RokLcw <lcwlyw@naver.com>
2022-10-11 17:17:04 +0900

    code clean

--- DVFR/View/Analysis_main.py
+++ DVFR/View/Analysis_main.py
@@ -1,9 +1,109 @@
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog
-from PyQt5.QtGui import QIcon    
+from PyQt5.QtGui import QIcon   
 
 class Analysis_main():
     def __init__(self, MainWindow):
+        self.load_item = QtWidgets.QListWidgetItem() # 불러온 영상 리스트
+        self.fileInfo_item = QtWidgets.QListWidgetItem() # File Info
+        self.Result_item = QtWidgets.QListWidgetItem()   # Result
+
+        self.ui_frame(MainWindow)   # ui 뼈대??
+        self.menubar(MainWindow)    # menubar
+    
+        
+        # --------------------- 불러온 영상 리스트 ---------------------
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(10, 10, 281, 541))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents_4 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_4.setGeometry(QtCore.QRect(0, 0, 279, 539))
+        self.scrollAreaWidgetContents_4.setObjectName("scrollAreaWidgetContents_4")
+        self.listWidget = QtWidgets.QListWidget(self.scrollAreaWidgetContents_4)
+        self.listWidget.setGeometry(QtCore.QRect(0, 0, 281, 541))
+        self.listWidget.setObjectName("listWidget")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents_4)
+
+        # --------------------- Tab widget ---------------------
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(300, 10, 621, 541))
+        self.tabWidget.setObjectName("tabWidget")
+
+        # --------------------- File Info ---------------------
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.scrollArea_2 = QtWidgets.QScrollArea(self.tab)
+        self.scrollArea_2.setGeometry(QtCore.QRect(0, 0, 621, 521))
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 619, 519))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.listWidget_2 = QtWidgets.QListWidget(self.scrollAreaWidgetContents)
+        self.listWidget_2.setGeometry(QtCore.QRect(0, 0, 621, 521))
+        self.listWidget_2.setObjectName("listWidget_2")
+        for i in range(0, 10):
+            
+            self.listWidget_2.addItem(self.fileInfo_item)
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents)
+        self.tabWidget.addTab(self.tab, "")
+
+        # --------------------- Result ---------------------
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.scrollArea_3 = QtWidgets.QScrollArea(self.tab_2)
+        self.scrollArea_3.setGeometry(QtCore.QRect(0, 0, 621, 521))
+        self.scrollArea_3.setWidgetResizable(True)
+        self.scrollArea_3.setObjectName("scrollArea_3")
+        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 619, 519))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+        self.listWidget_3 = QtWidgets.QListWidget(self.scrollAreaWidgetContents_2)
+        self.listWidget_3.setGeometry(QtCore.QRect(0, 0, 621, 521))
+        self.listWidget_3.setObjectName("listWidget_3")
+        for i in range(0, 10):
+            
+            self.listWidget_3.addItem(self.Result_item)
+        self.scrollArea_3.setWidget(self.scrollAreaWidgetContents_2)
+        self.tabWidget.addTab(self.tab_2, "")     
+
+        # ---------------------------------
+
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "DVFR"))
+        self.pushButton_2.setText(_translate("MainWindow", "삭제"))
+        self.pushButton.setText(_translate("MainWindow", "추출"))
+
+        # --------------------- File Info ---------------------
+        __sortingEnabled = self.listWidget_2.isSortingEnabled()
+        self.listWidget_2.setSortingEnabled(False)
+        # for i in range(0, 10):
+        #     item = self.listWidget_2.item(i)
+        #     item.setText(_translate("MainWindow", "새 항목"))
+        self.listWidget_2.setSortingEnabled(__sortingEnabled)
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "File Info"))
+        __sortingEnabled = self.listWidget_3.isSortingEnabled()
+
+        # --------------------- Result ---------------------
+        self.listWidget_3.setSortingEnabled(False)
+        # for i in range(0, 10):
+        #     item = self.listWidget_3.item(i)
+        #     item.setText(_translate("MainWindow", "새 항목"))
+        self.listWidget_3.setSortingEnabled(__sortingEnabled)
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Result"))
+
+        # --------------------- status bar ---------------------
+        self.menufile.setTitle(_translate("MainWindow", "file"))
+        self.menusetting.setTitle(_translate("MainWindow", "setting"))
+        self.menuabout.setTitle(_translate("MainWindow", "about"))
+        self.actionopen.setText(_translate("MainWindow", "open"))
+        self.actionabout_us.setText(_translate("MainWindow", "about us"))
+
+        self.tabWidget.setCurrentIndex(1)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def ui_frame(self, MainWindow):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
@@ -28,140 +128,65 @@ class Analysis_main():
         self.progressBar.setProperty("value", 24)
         self.progressBar.setObjectName("progressBar")
         self.horizontalLayout_2.addWidget(self.progressBar)
-        
-        # --------------------- 불러온 영상 리스트 ---------------------
-        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
-        self.scrollArea.setGeometry(QtCore.QRect(10, 10, 281, 541))
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setObjectName("scrollArea")
-        self.scrollAreaWidgetContents_4 = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents_4.setGeometry(QtCore.QRect(0, 0, 279, 539))
-        self.scrollAreaWidgetContents_4.setObjectName("scrollAreaWidgetContents_4")
-        self.listWidget = QtWidgets.QListWidget(self.scrollAreaWidgetContents_4)
-        self.listWidget.setGeometry(QtCore.QRect(0, 0, 281, 541))
-        self.listWidget.setObjectName("listWidget")
-        for i in range(0, 10):
-            item = QtWidgets.QListWidgetItem()
-            self.listWidget.addItem(item)
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents_4)
 
-        # --------------------- Tab widget ---------------------
-        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
-        self.tabWidget.setGeometry(QtCore.QRect(300, 10, 621, 541))
-        self.tabWidget.setObjectName("tabWidget")
-
-        # --------------------- File Info ---------------------
-        self.tab = QtWidgets.QWidget()
-        self.tab.setObjectName("tab")
-        self.scrollArea_2 = QtWidgets.QScrollArea(self.tab)
-        self.scrollArea_2.setGeometry(QtCore.QRect(0, 0, 621, 521))
-        self.scrollArea_2.setWidgetResizable(True)
-        self.scrollArea_2.setObjectName("scrollArea_2")
-        self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 619, 519))
-        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.listWidget_2 = QtWidgets.QListWidget(self.scrollAreaWidgetContents)
-        self.listWidget_2.setGeometry(QtCore.QRect(0, 0, 621, 521))
-        self.listWidget_2.setObjectName("listWidget_2")
-        for i in range(0, 10):
-            item = QtWidgets.QListWidgetItem()
-            self.listWidget_2.addItem(item)
-        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents)
-        self.tabWidget.addTab(self.tab, "")
-
-        # --------------------- Result ---------------------
-        self.tab_2 = QtWidgets.QWidget()
-        self.tab_2.setObjectName("tab_2")
-        self.scrollArea_3 = QtWidgets.QScrollArea(self.tab_2)
-        self.scrollArea_3.setGeometry(QtCore.QRect(0, 0, 621, 521))
-        self.scrollArea_3.setWidgetResizable(True)
-        self.scrollArea_3.setObjectName("scrollArea_3")
-        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 619, 519))
-        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
-        self.listWidget_3 = QtWidgets.QListWidget(self.scrollAreaWidgetContents_2)
-        self.listWidget_3.setGeometry(QtCore.QRect(0, 0, 621, 521))
-        self.listWidget_3.setObjectName("listWidget_3")
-        for i in range(0, 10):
-            item = QtWidgets.QListWidgetItem()
-            self.listWidget_3.addItem(item)
-        self.scrollArea_3.setWidget(self.scrollAreaWidgetContents_2)
-        self.tabWidget.addTab(self.tab_2, "")
-        
-        # --------------------- Under ---------------------
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 954, 26))
-        self.menubar.setObjectName("menubar")
-        self.menufile = QtWidgets.QMenu(self.menubar)
-        self.menufile.setObjectName("menufile")
-        self.menusetting = QtWidgets.QMenu(self.menubar)
-        self.menusetting.setObjectName("menusetting")
-        self.menuabout = QtWidgets.QMenu(self.menubar)
-        self.menuabout.setObjectName("menuabout")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+    def file_open(self, MainWindow):
+        # -------- File open --------
         self.actionopen = QtWidgets.QAction(MainWindow)
         self.actionopen.setObjectName("actionopen")
-        
-        # --------------------- status bar ---------------------
         openFile = QAction(QIcon('open.png'), 'Open', self.menufile)
         openFile.setShortcut('Ctrl+O')
         openFile.setStatusTip('Open New File')
         openFile.triggered.connect(self.showDialog)
 
-        
         self.menufile.addAction(openFile)
+
+
+    def showDialog(self):   # 불러온 영상 리스트
+        fname = QFileDialog.getOpenFileName(self.menufile, 'Open file', './')
+        self.listWidget.addItem(self.load_item)
+        self.load_item.setText(fname[0])
+
+    def menu_file(self, MainWindow):
+        self.menufile = QtWidgets.QMenu(self.menubar)
+        self.menufile.setObjectName("menufile")
+
+        # open file
+        self.file_open(MainWindow)
+
+    def menu_setting(self, MainWindow):
+        self.menusetting = QtWidgets.QMenu(self.menubar)
+        self.menusetting.setObjectName("menusetting")
+    
+    def menu_about(self, MainWindow):
+        self.menuabout = QtWidgets.QMenu(self.menubar)
+        self.menuabout.setObjectName("menuabout")
+
+        # about us
+        self.actionabout_us = QtWidgets.QAction(MainWindow)
+        self.actionabout_us.setObjectName("actionabout_us")
+        self.menuabout.addAction(self.actionabout_us)
+
+    def menubar(self, MainWindow):
+        # --------------------- menu bar ---------------------
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 954, 26))
+        self.menubar.setObjectName("menubar")
+        
+        # menu list
+        self.menu_file(MainWindow)
+        self.menu_setting(MainWindow)
+        self.menu_about(MainWindow)
+
+        # menubar
+        MainWindow.setMenuBar(self.menubar)
+        
+        # status bar
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        # menu bar action
         self.menubar.addAction(self.menufile.menuAction())
         self.menubar.addAction(self.menusetting.menuAction())
         self.menubar.addAction(self.menuabout.menuAction())
-        
-
-        self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(1)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "DVFR"))
-        self.pushButton_2.setText(_translate("MainWindow", "삭제"))
-        self.pushButton.setText(_translate("MainWindow", "추출"))
-
-        # --------------------- 불러온 영상 리스트 ---------------------
-        __sortingEnabled = self.listWidget.isSortingEnabled()
-        self.listWidget.setSortingEnabled(False)
-        for i in range(0, 10):
-            item = self.listWidget.item(i)
-            item.setText(_translate("MainWindow", "새 항목"))
-        self.listWidget.setSortingEnabled(__sortingEnabled)
-
-        # --------------------- File Info ---------------------
-        __sortingEnabled = self.listWidget_2.isSortingEnabled()
-        self.listWidget_2.setSortingEnabled(False)
-        for i in range(0, 10):
-            item = self.listWidget_2.item(i)
-            item.setText(_translate("MainWindow", "새 항목"))
-        self.listWidget_2.setSortingEnabled(__sortingEnabled)
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "File Info"))
-        __sortingEnabled = self.listWidget_3.isSortingEnabled()
-
-        # --------------------- Result ---------------------
-        self.listWidget_3.setSortingEnabled(False)
-        for i in range(0, 10):
-            item = self.listWidget_3.item(i)
-            item.setText(_translate("MainWindow", "새 항목"))
-        self.listWidget_3.setSortingEnabled(__sortingEnabled)
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Result"))
-
-        # --------------------- status bar ---------------------
-        self.menufile.setTitle(_translate("MainWindow", "file"))
-        self.menusetting.setTitle(_translate("MainWindow", "setting"))
-        self.menuabout.setTitle(_translate("MainWindow", "about"))
-        self.actionopen.setText(_translate("MainWindow", "open"))
-
-    def showDialog(self):
-        fname = QFileDialog.getOpenFileName(self.menufile, 'Open file', './')
-
-        print(fname[0])
