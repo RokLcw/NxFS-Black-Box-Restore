1b1f28be7f28b0181c476dd837f14ebd1f0148cd
RokLcw <lcwlyw@naver.com>
2022-10-11 16:43:58 +0900

    logo update

--- DVFR/main.py
+++ DVFR/main.py
@@ -1,15 +1,16 @@
 from View.Analysis_main import Analysis_main
-
+from PyQt5.QtGui import QIcon
 
 class MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(950, 650)
-
+        MainWindow.setWindowIcon(QIcon('main_logo.png'))
+    
         self.Analysis_main = Analysis_main(MainWindow)
 
 if __name__ == "__main__":
-    from PyQt5 import QtWidgets
+    from PyQt5 import QtWidgets, QtCore
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
